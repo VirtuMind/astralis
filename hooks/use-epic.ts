ec44b7597acdb7e12e63449edcbc8992c58037ba
Younes Khoubaz <youness.dobarim@gmail.com>
2025-10-28 18:06:59 +0100

    small bug

--- hooks/use-epic.ts
+++ hooks/use-epic.ts
@@ -6,7 +6,7 @@ import { NormalizedEPICItem } from "@/lib/types";
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export function useEPIC(date?: string) {
-  const params = date ?? `?date=${date}`;
+  const params = date ? `?date=${date}` : "";
   const { data, error, isLoading } = useSWR<NormalizedEPICItem[]>(
     `/api/epic${params}`,
     fetcher
